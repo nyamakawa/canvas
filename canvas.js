c6dffbf568647374a1107d2a8557a53d2abffc93
nyamakawa2 <nyamakawa2@gmail.com>
2014-12-04 18:18:01 +0900

    split function

--- canvas.js
+++ canvas.js
@@ -1,3 +1,26 @@
+function createImage(context, width, height) {
+    var image = {
+        context: context,
+        width: width,
+        height: height,
+    };
+
+    image.imageData = context.createImageData(image.width, image.height);
+    image.setPixel = function (x, y, r, g, b, a) {
+        var imageData = image.imageData;
+        var index = (x + y * imageData.width) * 4;
+        imageData.data[index+0] = r;
+        imageData.data[index+1] = g;
+        imageData.data[index+2] = b;
+        imageData.data[index+3] = a;
+    }
+    image.draw = function () {
+        image.context.putImageData(image.imageData, 0, 0);
+    }
+
+    return image;
+}
+
 window.onload = function () {
     var element = document.getElementById('section1');
     element.innerText = 'hello';
@@ -6,29 +29,6 @@ window.onload = function () {
     var context = canvas.getContext("2d");
     var height = canvas.height;
     var width = canvas.width;
-
-    function createImage(context, width, height) {
-        var image = {
-            context: context,
-            width: width,
-            height: height,
-        };
-
-        image.imageData = context.createImageData(image.width, image.height);
-        image.setPixel = function (x, y, r, g, b, a) {
-            var imageData = image.imageData;
-            var index = (x + y * imageData.width) * 4;
-            imageData.data[index+0] = r;
-            imageData.data[index+1] = g;
-            imageData.data[index+2] = b;
-            imageData.data[index+3] = a;
-        }
-        image.draw = function () {
-            image.context.putImageData(image.imageData, 0, 0);
-        }
-
-        return image;
-    }
 
     var image = createImage(context, width, height);
 
